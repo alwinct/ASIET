00d6b2641f2910571a6ba33d61916cb35f60f8ec
alwinct <alwinct004@gmail.com>
2024-08-17 12:13:11 +0530

    Update palindrome.java

--- OOPS/LAB/palindrome.java
+++ OOPS/LAB/palindrome.java
@@ -1,1 +1,29 @@
-
+//PALINDROME
+//ALWIN C T
+//ROLL NO-7
+import java.util.Scanner;
+class palindrome{
+public static void main(String[] args)
+{
+int n,rem,rev=0,temp;
+System.out.println("enter the value=");
+Scanner sc=new Scanner(System.in);
+n=sc.nextInt();
+temp=n;
+while(n!=0)
+{
+rem=n%10;
+rev=(rev*10)+rem;
+n=n/10;
+}
+if(rev==temp)
+{
+System.out.println("number is palindrome");
+}
+else
+{
+System.out.println("number is not palindrome");
+}
+sc.close();
+}
+}
