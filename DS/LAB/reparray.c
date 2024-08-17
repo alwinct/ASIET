62a3be46c499dfb78e66234df0a641fad5dd617b
alwinct <alwinct004@gmail.com>
2024-08-17 11:43:22 +0530

    Update reparray.c

--- DS/LAB/reparray.c
+++ DS/LAB/reparray.c
@@ -1,1 +1,18 @@
+//REPRESENTATION OF POLYNOMIAL USING ARRAY
+//ALWIN C T
+//ROLL NO-7
+#include <stdio.h>
 
+int main()
+
+{
+
+    int p1[3] = {6,2,5};
+
+    for(int i=2;i>=0;i--){
+
+        printf("%dx^%d+",p1[i],i);
+
+    }
+
+}
